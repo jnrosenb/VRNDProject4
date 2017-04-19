b0b5ce206e73102073cf7fba0b4810b1e47b2154
José Rosenbluth <Jos Rosenbluth>
2017-04-19 11:21:21 -0400

    fix: Last fix to git ignore, reuploading

---
+++ PuzzlerStarter/Assets/Scripts/GameLogic_UI.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLogic_UI : MonoBehaviour {
+	public GameObject player;
+	public GameObject startUI, restartUI;
+	public GameObject startPoint, playPoint, restartPoint;
+
+	public bool playerWon = false;
+
+	// Use this for initialization
+	void Start () {
+		player.transform.position = startPoint.transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0) && player.transform.position==playPoint.transform.position) {
+			puzzleSuccess ();
+		}
+	}
+
+	public void startPuzzle() { //Begin the puzzle sequence
+		toggleUI();
+		iTween.MoveTo (player, 
+			iTween.Hash (
+				"position", playPoint.transform.position, 
+				"time", 2, 
+				"easetype", "linear"
+			)
+		);
+
+	}
+
+	public void resetPuzzle() { //Reset the puzzle sequence
+		player.transform.position = startPoint.transform.position;
+	}
+
+
+	public void puzzleSuccess() { //Do this when the player gets it right
+		toggleUI();
+		iTween.MoveTo (player, 
+			iTween.Hash (
+				"position", restartPoint.transform.position, 
+				"time", 2, 
+				"easetype", "linear"
+			)
+		);
+	}
+	public void toggleUI() {
+		startUI.SetActive (!startUI.activeSelf);
+		restartUI.SetActive (!restartUI.activeSelf);
+	}
+
+}
---
+++ PuzzlerStarter/Assets/Scripts/startButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startButton : MonoBehaviour {
+
+	public Camera mainCam;
+	public GameObject waypoint;
+
+	public void OnClicked()
+	{
+		Debug.Log ("Clicked the button");
+		mainCam.transform.position = waypoint.transform.position;
+	}
+
+	public void OnHover()
+	{
+		Debug.Log ("Hovering oVer");
+	}
+}
